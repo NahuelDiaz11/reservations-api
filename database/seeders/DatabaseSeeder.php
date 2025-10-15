65c8e3c69f7ca26263a34463bbd10e64c3dd67be
NahuelDiaz11 <nahuel.kp2012@gmail.com>
2025-10-15 18:28:14 -0300

    se agrego el factory de reservas en databaseSeeder para que se ejecute cuando se ejecute los seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Reservation;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -21,5 +22,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserSeeder::class,
         ]);
+        Reservation::factory(50)->create();
+        
     }
 }
